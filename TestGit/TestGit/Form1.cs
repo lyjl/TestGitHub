1ae08ebaf61d8f58adefd54a22a0120c49a11b39
冷翊 <772208788@QQ.com>
2019-08-02 10:28:39 +0800

    更改

---
+++ TestGit/TestGit/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TestGit
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Convert.ToDecimal(0);
+            Convert.ToString(0);
+            Convert.ToString(0);
+        }
+    }
+}
